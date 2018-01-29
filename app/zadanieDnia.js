65c0ce376c8d7616020363e51808d039628b61a3
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2018-01-29 16:37:47 +0100

    Zadanie dnia start

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,23 @@
 //Twój kod
+
+let numbers = [];
+
+this.numbers = process.argv.slice(2)
+
+console.log(this.numbers);
+
+this.numbers.map((x, i) => {
+    y = parseInt(x);
+    this.numbers.splice(i, 1, y);
+});
+
+console.log(this.numbers.sort());
+
+let timeout = 0;
+const intervalId = setTimeout(() => {
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i])
+    timeout = numbers[i]*1000;
+}
+
+}, timeout)
